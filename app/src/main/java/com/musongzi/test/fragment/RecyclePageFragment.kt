ccc440f3d495237ee0b84c686fb61ad685adb67d
linhui <536451207@qq.com>
2024-08-07 17:45:38 +0800

    完善rx与viewmodel生命周期

--- app/src/main/java/com/musongzi/test/fragment/RecyclePageFragment.kt
+++ app/src/main/java/com/musongzi/test/fragment/RecyclePageFragment.kt
@@ -14,16 +14,20 @@ import com.google.android.exoplayer2.upstream.DataSource
 import com.google.android.exoplayer2.upstream.DefaultDataSource
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
 import com.musongzi.core.ExtensionCoreMethod.adapter
+import com.musongzi.core.ExtensionCoreMethod.getApi
 import com.musongzi.core.ExtensionCoreMethod.linearLayoutManager
 import com.musongzi.core.ExtensionCoreMethod.liveSaveStateObserver
 import com.musongzi.core.ExtensionCoreMethod.refreshLayoutInit
+import com.musongzi.core.StringChooseBean
 import com.musongzi.core.base.bean.IUserInfo
 import com.musongzi.core.base.fragment.DataBindingFragment
 import com.musongzi.core.base.page2.PageLoader
 import com.musongzi.core.base.page2.RequestObservableBean
 import com.musongzi.core.base.page2.SimplePageCall
 import com.musongzi.core.itf.page.IPageEngine
+import com.musongzi.test.MszTestApi
 import com.musongzi.test.bean.ListDataBean
+import com.musongzi.test.bean.ResponeCodeBean
 import com.musongzi.test.bean.UserInfo
 import com.musongzi.test.databinding.FragmentRecyclePageBinding
 import com.musongzi.test.databinding.ItemUserInfoBinding
@@ -42,7 +46,7 @@ private const val ARG_PARAM2 = "param2"
  */
 class RecyclePageFragment : DataBindingFragment<FragmentRecyclePageBinding>() {
 
-    private var pageLoad: IPageEngine<UserInfo>? = null
+    private var pageLoad: IPageEngine<StringChooseBean>? = null
     val viewmodel: ListDataViewModel by viewModels()
 
     override fun notifyDataSetChangedItem(postiont: Int) {
@@ -75,48 +79,51 @@ class RecyclePageFragment : DataBindingFragment<FragmentRecyclePageBinding>() {
                             safePlayer().play()
                         }
 //                        notifyDataSetChanged()
-                        viewmodel.loadDataUser()
+
                     }
                 }
             }
         }
     }
 
-    private fun safeGetPageEngine(): IPageEngine<UserInfo> {
+    private fun safeGetPageEngine(): IPageEngine<StringChooseBean> {
         if (pageLoad == null) {
             pageLoad = PageLoader.createInstance(object :
-                SimplePageCall<UserInfo>(this@RecyclePageFragment) {
-                override fun getRemoteData(page: Int, pageSize: Int): Observable<List<UserInfo>>? {
+                SimplePageCall<StringChooseBean>(this@RecyclePageFragment) {
+                override fun getRemoteData(page: Int, pageSize: Int): Observable<List<StringChooseBean>>? {
                     Log.i(TAG, "getRemoteData: page = $page , pageSize = $pageSize")
-                    return Observable.create {
-                        val dataBean = ListDataBean<UserInfo>()
-                        it.onNext(dataBean.data.apply {
-
-
-                            if(page == 0) {
-                                add(UserInfo(System.currentTimeMillis(), "小米"))
-                                add(UserInfo(System.currentTimeMillis(), "小明"))
-                                add(UserInfo(System.currentTimeMillis(), "小白"))
-                                add(UserInfo(System.currentTimeMillis(), "小红", gender = IUserInfo.WOMAN))
-                                add(UserInfo(System.currentTimeMillis(), "小寻", gender = IUserInfo.WOMAN))
-                                add(UserInfo(System.currentTimeMillis(), "小志"))
-                                add(UserInfo(System.currentTimeMillis(), "小薰", gender = IUserInfo.WOMAN))
-                                add(UserInfo(System.currentTimeMillis(), "小强"))
-                                add(UserInfo(System.currentTimeMillis(), "小赞"))
-                                add(UserInfo(System.currentTimeMillis(), "小芸", gender = IUserInfo.WOMAN))
-                            }else if(page == 1) {
-                                add(UserInfo(System.currentTimeMillis(), "小欧"))
-                                add(UserInfo(System.currentTimeMillis(), "小林"))
-                                add(UserInfo(System.currentTimeMillis(), "小晨", gender = IUserInfo.WOMAN))
-                                add(UserInfo(System.currentTimeMillis(), "小婷", gender = IUserInfo.WOMAN))
-                                add(UserInfo(System.currentTimeMillis(), "小琪", gender = IUserInfo.WOMAN))
-                                add(UserInfo(System.currentTimeMillis(), "小国"))
-                                add(UserInfo(System.currentTimeMillis(), "小海"))
-                                add(UserInfo(System.currentTimeMillis(), "小痞"))
-                            }
-                        })
-                        it.onComplete()
+                    return getApi<MszTestApi>().getArrayEngine(page,pageSize).map {
+                        it.data
                     }
+//                    return Observable.create {
+//                        val dataBean = ListDataBean<UserInfo>()
+//                        it.onNext(dataBean.data.apply {
+//
+//
+//                            if(page == 0) {
+//                                add(UserInfo(System.currentTimeMillis(), "小米"))
+//                                add(UserInfo(System.currentTimeMillis(), "小明"))
+//                                add(UserInfo(System.currentTimeMillis(), "小白"))
+//                                add(UserInfo(System.currentTimeMillis(), "小红", gender = IUserInfo.WOMAN))
+//                                add(UserInfo(System.currentTimeMillis(), "小寻", gender = IUserInfo.WOMAN))
+//                                add(UserInfo(System.currentTimeMillis(), "小志"))
+//                                add(UserInfo(System.currentTimeMillis(), "小薰", gender = IUserInfo.WOMAN))
+//                                add(UserInfo(System.currentTimeMillis(), "小强"))
+//                                add(UserInfo(System.currentTimeMillis(), "小赞"))
+//                                add(UserInfo(System.currentTimeMillis(), "小芸", gender = IUserInfo.WOMAN))
+//                            }else if(page == 1) {
+//                                add(UserInfo(System.currentTimeMillis(), "小欧"))
+//                                add(UserInfo(System.currentTimeMillis(), "小林"))
+//                                add(UserInfo(System.currentTimeMillis(), "小晨", gender = IUserInfo.WOMAN))
+//                                add(UserInfo(System.currentTimeMillis(), "小婷", gender = IUserInfo.WOMAN))
+//                                add(UserInfo(System.currentTimeMillis(), "小琪", gender = IUserInfo.WOMAN))
+//                                add(UserInfo(System.currentTimeMillis(), "小国"))
+//                                add(UserInfo(System.currentTimeMillis(), "小海"))
+//                                add(UserInfo(System.currentTimeMillis(), "小痞"))
+//                            }
+//                        })
+//                        it.onComplete()
+//                    }
                 }
 
                 override fun pageSize(): Int = 10
@@ -127,8 +134,8 @@ class RecyclePageFragment : DataBindingFragment<FragmentRecyclePageBinding>() {
                 }
 
                 override fun handlerDataChange(
-                    data: MutableList<UserInfo>,
-                    request: RequestObservableBean<List<UserInfo>>
+                    data: MutableList<StringChooseBean>,
+                    request: RequestObservableBean<List<StringChooseBean>>
                 ) {
                     Log.i(TAG, "handlerDataChange: data.size = ${data.size}")
                     dataBinding.idRecyclerView.adapter?.notifyDataSetChanged()
